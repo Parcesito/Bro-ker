45f5ca22ae504480729e21d16cc7446d581a6fd7
Parcesito <diederiiik@gmail.com>
2025-05-05 00:08:07 -0500

    Creation commit

---
+++ Models/Basic_LSTM.py
@@ -0,0 +1,8 @@
+"""
+En este archivo se contienen las funciones de creación y entrenamiento del modelo LSTM básico,
+entrenado utilizando los valores de cierre de las acciones en bolsa.
+"""
+
+"""
+Actualmente el modelo se encuentra en su versión 0.1, una beta funcional.
+"""
